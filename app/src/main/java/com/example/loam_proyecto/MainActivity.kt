c618a668551285ce7e0945086001973abc88d315
nicoolukes <nicolasslukes9@gmail.com>
2025-08-22 17:23:14 -0300

    initial

---
+++ app/src/main/java/com/example/loam_proyecto/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.loam_proyecto
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.loam_proyecto.ui.theme.LOAMProyectoTheme
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LOAMProyectoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("mensaje")
+
+        // Escribe un valor en la base
+        myRef.setValue("Hola yara")
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LOAMProyectoTheme {
+        Greeting("Android")
+    }
+}
